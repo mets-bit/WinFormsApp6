722597434becb8cfdcc6ad59b2ab7ea45005f1c0
John Paul Metica <metica.131860@carmona.sti.edu.ph>
2023-09-27 17:08:02 +0800

    aasdas

--- WinFormsApp6/Form1.Designer.cs
+++ WinFormsApp6/Form1.Designer.cs
@@ -54,19 +54,22 @@
             this.tableLayoutPanel1.Controls.Add(this.groupBox3, 2, 0);
             this.tableLayoutPanel1.Controls.Add(this.groupBox2, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(-55, 126);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(33, 168);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(576, 273);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(542, 322);
             this.tableLayoutPanel1.TabIndex = 5;
             // 
             // groupBox4
             // 
             this.groupBox4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.groupBox4.Location = new System.Drawing.Point(354, 59);
+            this.groupBox4.Location = new System.Drawing.Point(328, 58);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(97, 154);
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox4.Size = new System.Drawing.Size(101, 205);
             this.groupBox4.TabIndex = 3;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "groupBox4";
@@ -74,9 +77,11 @@
             // groupBox3
             // 
             this.groupBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.groupBox3.Location = new System.Drawing.Point(239, 59);
+            this.groupBox3.Location = new System.Drawing.Point(220, 58);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(97, 154);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox3.Size = new System.Drawing.Size(101, 205);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "groupBox3";
@@ -84,9 +89,11 @@
             // groupBox2
             // 
             this.groupBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.groupBox2.Location = new System.Drawing.Point(124, 59);
+            this.groupBox2.Location = new System.Drawing.Point(112, 58);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(97, 154);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Size = new System.Drawing.Size(101, 205);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "groupBox2";
@@ -94,9 +101,11 @@
             // groupBox1
             // 
             this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.groupBox1.Location = new System.Drawing.Point(9, 59);
+            this.groupBox1.Location = new System.Drawing.Point(4, 58);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(97, 154);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(101, 205);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
@@ -104,26 +113,27 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(835, 92);
+            this.label2.Location = new System.Drawing.Point(954, 123);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 15);
+            this.label2.Size = new System.Drawing.Size(96, 20);
             this.label2.TabIndex = 11;
             this.label2.Text = "Inactive View";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(200, 92);
+            this.label1.Location = new System.Drawing.Point(229, 123);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 15);
+            this.label1.Size = new System.Drawing.Size(86, 20);
             this.label1.TabIndex = 10;
             this.label1.Text = "Active View";
             // 
             // Next_Btn
             // 
-            this.Next_Btn.Location = new System.Drawing.Point(898, 443);
+            this.Next_Btn.Location = new System.Drawing.Point(1026, 591);
+            this.Next_Btn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Next_Btn.Name = "Next_Btn";
-            this.Next_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Next_Btn.Size = new System.Drawing.Size(86, 31);
             this.Next_Btn.TabIndex = 9;
             this.Next_Btn.Text = "Next";
             this.Next_Btn.UseVisualStyleBackColor = true;
@@ -131,9 +141,10 @@
             // 
             // Prev_Btn
             // 
-            this.Prev_Btn.Location = new System.Drawing.Point(116, 443);
+            this.Prev_Btn.Location = new System.Drawing.Point(133, 591);
+            this.Prev_Btn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Prev_Btn.Name = "Prev_Btn";
-            this.Prev_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Prev_Btn.Size = new System.Drawing.Size(86, 31);
             this.Prev_Btn.TabIndex = 7;
             this.Prev_Btn.Text = "Prev";
             this.Prev_Btn.UseVisualStyleBackColor = true;
@@ -153,24 +164,26 @@
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(589, 126);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(677, 168);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 1;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(576, 273);
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(654, 322);
             this.tableLayoutPanel3.TabIndex = 8;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1066, 534);
+            this.ClientSize = new System.Drawing.Size(1392, 712);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Next_Btn);
             this.Controls.Add(this.Prev_Btn);
             this.Controls.Add(this.tableLayoutPanel3);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
